5e978033a94a2671d2c7fb42a25a518046427c4d
nicapotato <nick.brooks27@gmail.com>
2026-07-30 16:56:42 +0100

    add maps act-2

---
+++ resources/experimental/16_tile_dualgrid_tilesheet-64x.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="16_tile_dualgrid_tilesheet-64x" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <transformations hflip="1" vflip="1" rotate="1" preferuntransformed="0"/>
+ <image source="../16_tile_dualgrid_tilesheet-64x.png" width="128" height="128"/>
+ <wangsets>
+  <wangset name="15tile-dualset" type="corner" tile="-1">
+   <wangcolor name="red" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="1" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="2" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="3" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="4" wangid="0,0,0,1,0,0,0,1"/>
+   <wangtile tileid="5" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="6" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="7" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="8" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="9" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="10" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="11" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="13" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="14" wangid="0,1,0,0,0,1,0,0"/>
+   <wangtile tileid="15" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
---
+++ resources/experimental/5tileset-dualgrid-1.tsx
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="5tileset-dualgrid-1" tilewidth="16" tileheight="16" tilecount="5" columns="1">
+ <transformations hflip="1" vflip="1" rotate="1" preferuntransformed="0"/>
+ <image source="../tilesets/5tileset-dualgrid-1.png" width="16" height="80"/>
+ <wangsets>
+  <wangset name="5tiledualset-1" type="mixed" tile="-1">
+   <wangcolor name="green" color="#00ed11" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="1" wangid="0,1,0,0,0,1,0,0"/>
+   <wangtile tileid="2" wangid="0,0,0,0,0,1,1,1"/>
+   <wangtile tileid="3" wangid="0,0,0,1,1,1,1,1"/>
+   <wangtile tileid="4" wangid="1,1,1,1,1,1,1,1"/>
+  </wangset>
+  <wangset name="5tiledualsetcorner-1" type="corner" tile="-1">
+   <wangcolor name="green" color="#00ed4e" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="1" wangid="0,1,0,0,0,1,0,0"/>
+   <wangtile tileid="2" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="3" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="4" wangid="0,1,0,1,0,1,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
